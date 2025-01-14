8e8961320b38d682d35852e75816422121a3eedb
odudename <odude.com@gmail.com>
2025-01-14 22:45:19 +0545

    1.63

--- src/Reusables/helper.tsx
+++ src/Reusables/helper.tsx
@@ -36,7 +36,7 @@ export const Logo = ({ onToggle, isOpen }: any) => {
     <HStack>
       <>
         <Link
-          href={"/#1.62"}
+          href={"/#1.63"}
           textAlign={textAlign as any}
           fontFamily={"heading"}
           color={textColor}
